722f5bd9c61fb2d74a8048d10121a2b8befb9dc4
wtp30twice <floweryfruitbasket@gmail.com>
2026-02-11 14:05:08 -0600

    Update validation.ts

--- src/actors/groupMedia/validation.ts
+++ src/actors/groupMedia/validation.ts
@@ -5,6 +5,7 @@ import type { FbGroupMemberActorInput } from './config';
 const inputValidationSchema = Joi.object<FbGroupMemberActorInput>({
   ...allActorInputValidationFields,
   maxMembers: Joi.number().integer().min(0).allow(null).optional(),
+  cookies: Joi.string().optional(),
 } as any);
 
 export const validateInput = (input: FbGroupMemberActorInput | null) => {
